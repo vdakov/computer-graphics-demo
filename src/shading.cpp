02c8fb0fde1f21743247c1a52db8af50d914f837
Berken Tekin <tekin.berken01@gmail.com>
2022-10-27 15:18:14 +0200

    Phong Shading (no visual debug)

--- src/shading.cpp
+++ src/shading.cpp
@@ -3,27 +3,25 @@
 #include <glm/geometric.hpp>
 #include <shading.h>
 
-
-template <typename T>
-T clampNumber(T num, T lo, T hi)
-{
-    return std::max(lo, std::min(hi, num));
-}
-
+// #define CSDEBUG
 
 const glm::vec3 computeShading(const glm::vec3& lightPosition, const glm::vec3& lightColor, const Features& features, Ray ray, HitInfo hitInfo)
 {
     // TODO: implement the Phong shading model.
-    //const float nlprod = clampNumber(glm::dot(hitInfo.normal, lightPosition - hitInfo.texCoord), 0.0f, 1.0f);
+    //const float nlprod = std::clamp(glm::dot(hitInfo.normal, lightPosition - hitInfo.texCoord), 0.0f, 1.0f);
     //return lightColor * materialInformation.Kd * nlprod;
     if (features.enableShading) {
         glm::vec3 intersectionPoint { ray.origin + ray.direction * ray.t };
-        Ray L { .origin = lightPosition, 
-            .direction = -glm::normalize(lightPosition - intersectionPoint), 
+        glm::vec3 dir { glm::normalize(lightPosition - intersectionPoint) };
+        Ray L { .origin = intersectionPoint +  dir * powf(10, -5),
+            .direction = dir,
             .t = glm::length(lightPosition - intersectionPoint) };
-        Ray& V { ray };
-        Ray R { computeReflectionRay(L, hitInfo) };
-        return lightColor * hitInfo.material.ks * glm::pow(clampNumber(glm::dot(V.direction, R.direction), 0.0f, 1.0f), 2.0f);
+
+        const Ray& V { ray };
+        Ray R { computeReflectionRay(L, hitInfo) }; // TODO get rid of glm::reflect
+        glm::vec3 diffuse { lightColor * hitInfo.material.kd * std::clamp(glm::dot(L.direction, hitInfo.normal), 0.0f, 1.0f) };
+        glm::vec3 specular { lightColor * hitInfo.material.ks * glm::pow(std::clamp(glm::dot(V.direction, R.direction), 0.0f, 1.0f), hitInfo.material.shininess) };
+        return diffuse + specular;
     }
     return hitInfo.material.kd;
 }
@@ -31,11 +29,22 @@ const glm::vec3 computeShading(const glm::vec3& lightPosition, const glm::vec3& 
 
 const Ray computeReflectionRay (Ray ray, HitInfo hitInfo)
 {
+
     // Do NOT use glm::reflect!! write your own code.
-    glm::vec3 intersectionPoint { ray.origin + ray.direction * ray.t };
-    Ray reflectionRay { .origin = intersectionPoint,
-        .direction = ray.direction - 2.0f * hitInfo.normal * glm::dot(ray.direction, hitInfo.normal),
-        .t=ray.t };
+    glm::vec3 intersectionPoint { ray.origin };
+    glm::vec3 reflectDir { glm::normalize(ray.direction - 2.0f * hitInfo.normal * glm::dot(ray.direction, hitInfo.normal)) };
+    #ifndef CSDEBUG
+    Ray reflectionRay { .origin = intersectionPoint + reflectDir * powf(10, -5),
+        .direction = reflectDir,
+        .t=FLT_MAX };
+    #endif
+    #ifdef CSDEBUG
+    Ray reflectionRay { 
+        .origin=intersectionPoint + reflectDir * powf(10, -5), 
+        .direction=glm::reflect(-ray.direction, hitInfo.normal), 
+        .t=ray.t 
+    };
+    #endif
     // TODO: implement the reflection ray computation.
     return reflectionRay;        
 }
